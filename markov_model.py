0ff3c571a8689781d8cb5b03e058783105b3609b
IanChar <ianchar4@gmail.com>
2016-11-01 19:42:59 -0600

    Changed model to use multinomial

--- markov_model.py
+++ markov_model.py
@@ -2,7 +2,7 @@
 from __future__ import division
 import operator as op
 import math
-from random import random
+import random
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -15,38 +15,12 @@ def autoamta_sim(word_length, alphabet_size):
     return curr_sizes
 
 def k_sim(bins, marbles):
-    """Simulate a value of k given the specifications. """
-    probs = [k_pdf(bins, marbles, k) for k in range(1, min(bins, marbles) + 1)]
-    curr, curr_prob = 1, probs[0]
-    rand_num = random()
-    while rand_num > curr_prob:
-        curr += 1
-        try:
-            curr_prob += probs[curr - 1]
-        except:
-            print "PROBLEM", curr, probs, rand_num
-            curr = len(probs)
-            rand_num = -1
-    return curr
-
-
-def k_pdf(bins, marbles, selected):
-    """Calculate the probability K takes thes selected value."""
-    stir_num = float(0)
-    for j in range(selected + 1):
-        stir_num += (-1) ** (selected - j) * ncr(selected, j) * j ** marbles
-    stir_num /= math.factorial(selected)
-
-    return (float(ncr(bins, selected) * stir_num * math.factorial(selected))
-            / (bins ** marbles))
-
-def ncr(n, r):
-    """ Compute choose (from stack overflow) """
-    r = min(r, n-r)
-    if r == 0: return 1
-    numer = reduce(op.mul, xrange(n, n-r, -1))
-    denom = reduce(op.mul, xrange(1, r+1))
-    return float(numer//denom)
+    simulated = np.random.multinomial(marbles, [1/float(bins)]*bins)
+    num_full = 0
+    for urn in simulated:
+        if urn > 0:
+            num_full += 1
+    return num_full
 
 def make_hist(data):
     _, _, _ = plt.hist(data, 75, facecolor='blue', alpha=0.5)
@@ -63,4 +37,4 @@ def failure_trials(trials):
     return data
 
 if __name__ == '__main__':
-    make_hist(failure_trials(10000))
+    make_hist(failure_trials(1000))
